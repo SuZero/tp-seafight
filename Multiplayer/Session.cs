b4b1090c590a01fbbfb0315b9aace1b4721106cf
ArtemSynth <kirktomblack93@gmail.com>
2015-12-06 22:22:10 +0400

    add code

--- Multiplayer/Session.cs
+++ Multiplayer/Session.cs
@@ -8,10 +8,67 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
 
 namespace Multiplayer
 {
-    public class Class1
+    public class Session : Microsoft.Xna.Framework.Game
     {
+        NetworkSession networkSession;
+        //Для записи и чтения пакетов данных
+        PacketWriter packetWriter;
+        PacketReader packetReader;
+
+        private AvailableNetworkSessionCollection availNetSessions = NetworkSession.Find(NetworkSessionType.SystemLink, 2, null);
+
+
+
+        public void createRoom()
+        {
+            networkSession = NetworkSession.Create(NetworkSessionType.SystemLink, 1, 2);
+            //Подключаем обработчики событий
+            joinEventHandlers();
+        }
+
+        public void joinInRoom()
+        {
+            networkSession = NetworkSession.Join(availNetSessions[0]);
+            joinEventHandlers();
+        }
+
+
+        //Подключение обработчиков событий
+        void joinEventHandlers()
+        {
+            //Игрок вошел в комнату
+            networkSession.GamerJoined += new EventHandler<GamerJoinedEventArgs>(networkSession_GamerJoined);
+            //Сессия закрылась
+            networkSession.SessionEnded += new EventHandler<NetworkSessionEndedEventArgs>(networkSession_SessionEnded);
+        }
+        //Обработчик события, происходящего при присоединении нового игрока
+        void networkSession_GamerJoined(object sender, GamerJoinedEventArgs e)
+        {
+            // Номер игрока
+            int player = networkSession.AllGamers.IndexOf(e.Gamer);
+
+            // e.Gamer.Tag = Создать объект
+        }
+
+        //Обработчик события, происходящего при закрытии сессии
+        void networkSession_SessionEnded(object sender, NetworkSessionEndedEventArgs e)
+        {
+            //уничтожим сессию
+            networkSession.Dispose();
+            networkSession = null;
+        }
+
+        //Процедура обработки событий в течение сессии
+        void WorkWithSession()
+        {
+
+        }
+
+
+
     }
 }
