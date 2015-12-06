42a9aaefc8369874b59dfb7fd83077331d402239
ArtemSynth <kirktomblack93@gmail.com>
2015-12-07 00:01:10 +0400

    z

--- BattleField/Ship.cs
+++ BattleField/Ship.cs
@@ -1,7 +1,34 @@
-﻿namespace BattleField
+﻿using System;
+using System.Linq;
+
+namespace BattleField
 {
-    public class Ship
+    public class Ship : IShip
     {
-         
+        private int length;
+        private bool[] sectionDestroyed;
+
+        public int Length { get { return length; } }
+        public bool IsSunk { get { return sectionDestroyed.All(b => b); } }
+
+        public Ship(int shipSize)
+        {
+            length = shipSize;
+            sectionDestroyed = (bool[])Array.CreateInstance(typeof(Boolean), shipSize);
+        }
+        public void Hit(int section)
+        {
+            sectionDestroyed[section] = true;
+        }
+        public Placement SailTo(int x, int y, Orientation orientation)
+        {
+            return new Placement(this, x, y, orientation);
+        }
+        public Placement SailTo(Coordinate coord, Orientation orientation)
+        {
+            return new Placement(this, coord, orientation);
+        }
+
+
     }
 }
--- Control/ShotFeedback.cs
+++ Control/ShotFeedback.cs
@@ -2,6 +2,16 @@
 {
     public class ShotFeedback
     {
-         
+        private int hits;
+        private int sunkShips;
+
+        public int Hits { get { return hits; } }
+        public int SunkShips { get { return sunkShips; } }
+
+        public ShotFeedback(int hits, int sunkShips)
+        {
+            hits = hits;
+            sunkShips = sunkShips;
+        }
     }
 }
